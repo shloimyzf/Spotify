ddf46841020fe714e978fb4ed30d3a380bb133e1
shloimyzf <shloimyf33107@gmail.com>
2026-06-22 18:41:40 -0400

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'streampulse-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
+  );
+});
